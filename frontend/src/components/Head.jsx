35ffc0510538cc6d235c7796799e7267c11396c3
Taylor J <taylordjone@gmail.com>
2023-09-18 17:42:40 +0200

    adding cart badge

--- frontend/src/components/Head.jsx
+++ frontend/src/components/Head.jsx
@@ -1,29 +1,37 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { LinkContainer } from "react-router-bootstrap";
-import logo from "../assets/logo.png";
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
+import logo from '../assets/logo.png';
 
 const Head = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
+          <LinkContainer to='/'>
             <Navbar.Brand>
-              <img src={logo} alt="BotanicBeauty" />
+              <img src={logo} alt='BotanicBeauty' />
               BotanicBeauty
             </Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/cart">
+          <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart />
                   Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((acc, curr) => acc + curr.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
+              <LinkContainer to='/login'>
                 <Nav.Link>
                   <FaUser />
                   Sign in
