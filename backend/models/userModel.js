038f0e4d6c77522ba920be7193730e0a421bac31
Taylor J <taylordjone@gmail.com>
2023-09-20 13:09:00 +0200

    adding token

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
@@ -26,6 +27,10 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
 
 export default User;
