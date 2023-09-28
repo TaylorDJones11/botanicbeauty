cd273e8e9b95ac62a34b12e2c906c676e0bdbcd8
Taylor J <taylordjone@gmail.com>
2023-09-28 13:08:33 +0200

    order routes and controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -26,12 +26,28 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @route GET/api/orders/:id/pay
 // @access Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send('get order to paid');
+  res.send('update order to paid');
 });
 
 // @desc Update order to delivered
 // @route GET/api/orders/:id/deliver
-// @access Private
+// @access Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send('get order to delivered');
 });
+
+// @desc Get all orders
+// @route GET/api/orders
+// @access Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  res.send('get all orders');
+});
+
+export {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToDelivered,
+  updateOrderToPaid,
+  getOrders,
+};
