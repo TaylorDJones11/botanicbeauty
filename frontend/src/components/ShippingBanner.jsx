2f4bfd7a940edf8e4acfc56072331f8ff69e062e
Taylor J <taylordjone@gmail.com>
2023-10-10 18:50:24 +0200

    adding custom scss colors

--- frontend/src/components/ShippingBanner.jsx
+++ frontend/src/components/ShippingBanner.jsx
@@ -1,6 +1,6 @@
 const ShippingBanner = () => {
   return (
-    <p className='text-center text-bg-info py-2 fw-semibold'>
+    <p className='text-center text-bg-info py-2 fw-normal'>
       FREE STANDARD SHIPPING ON ORDERS $40+
     </p>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 // import "bootstrap/dist/css/bootstrap.min.css";
 import './assets/styles/bootstrap.custom.css';
+import './assets/styles/custom.scss';
 import './assets/styles/index.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
