216db8887fc6b985098a754443f1e763c9290af6
Taylor J <taylordjone@gmail.com>
2023-10-16 16:49:43 +0200

    fixing product cards

--- frontend/src/components/ShippingBanner.jsx
+++ frontend/src/components/ShippingBanner.jsx
@@ -1,6 +1,9 @@
 const ShippingBanner = () => {
   return (
-    <p className='text-center text-bg-info py-2 fw-normal'>
+    <p
+      className='text-center text-bg-info py-2 fw-normal'
+      style={{ margin: 0 }}
+    >
       FREE STANDARD SHIPPING ON ORDERS $40+
     </p>
   );
